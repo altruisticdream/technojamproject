f633de6361f8f4f3ff18d5907c8ecaeafd38bdc5
altruisticdream <ashutoshgautam9628@outlook.com>
2020-08-09 11:56:16 +0530

    TECHNOJAM TASK

---
+++ app/src/main/java/com/example/workground/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.workground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
